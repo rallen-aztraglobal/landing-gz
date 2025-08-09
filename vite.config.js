2eedb620604833bc8b32195bdee4b8def682bc90
allen <allen.yho@yahoo.com>
2025-08-09 14:31:59 +0800

    集成prettier

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import autoprefixer from "autoprefixer";
 import pxToRem from "postcss-pxtorem";
+import htmlMinify from 'vite-plugin-html-minify';
+import compression from 'vite-plugin-compression';
 import path from "path";
 
 export default defineConfig(({ mode }) => {
@@ -10,6 +12,25 @@ export default defineConfig(({ mode }) => {
         root: path.resolve(__dirname, "src"),  // 让开发和入口都在 src 目录
         base: "./",                            // 打包资源相对路径
         publicDir: path.resolve(__dirname, "public"), // 静态资源放项目根的 public
+        plugins: [
+            htmlMinify({
+                minifyOptions: {
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    removeRedundantAttributes: true,
+                    removeEmptyAttributes: true,
+                    minifyCSS: true,
+                    minifyJS: true,
+                }
+            }),
+            compression({
+                verbose: true,       // 控制台输出压缩结果
+                disable: false,      // 启用压缩
+                threshold: 10240,    // 超过 10kb 才压缩
+                algorithm: 'gzip',   // 压缩算法，支持 gzip、brotliCompress 等
+                ext: '.gz',          // 生成文件后缀名
+            }),
+        ],
         build: {
             outDir: path.resolve(__dirname, "dist"), // 打包产物输出到项目根的 dist
             emptyOutDir: true,
@@ -35,7 +56,6 @@ export default defineConfig(({ mode }) => {
                     entryFileNames: "scripts/[name]-[hash].js",
                     manualChunks(id) {
                         // 这里判断路径，把 config.js 单独拆成一个 chunk
-                        console.log(id)
                         if (id.includes("config.js")) {
                             return 'config'
                         }
