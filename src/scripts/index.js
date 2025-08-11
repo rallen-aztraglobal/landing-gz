e61275f15e0de54d5b496336128931cd42a1e72e
allen <allen.yho@yahoo.com>
2025-08-11 16:55:29 +0800

    api 下载

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,9 @@ import QRCode from 'qrcode'
 
 function handleDownloadApk() {
   const a = document.createElement('a')
-  a.href = window.downloadUrl
-  // a.download = 'app.apk'; // 下载文件名
+  // 直接访问 nginx 代理的固定路径
+  a.href = `${location.origin}/api/download/apk`
+  a.style.display = 'none'
   document.body.appendChild(a)
   a.click()
   document.body.removeChild(a)
@@ -11,10 +12,15 @@ function handleDownloadApk() {
 
 window.onload = function () {
   const canvas = document.getElementById('qrcode')
-  QRCode.toCanvas(canvas, window.location.href,    {
-    margin: 1 // 控制白边宽度（单位是模块数，默认是 4）
-  },function (error) {
-    if (error) console.error(error)
-  })
+  QRCode.toCanvas(
+    canvas,
+    window.location.href,
+    {
+      margin: 1, // 控制白边宽度（单位是模块数，默认是 4）
+    },
+    function (error) {
+      if (error) console.error(error)
+    },
+  )
   document.getElementById('download').addEventListener('click', handleDownloadApk)
 }
