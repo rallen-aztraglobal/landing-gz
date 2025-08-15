ef5a475839d570232990a40b382f80fb5d930ed3
allen <allen.yho@yahoo.com>
2025-08-15 12:44:27 +0800

    gtag

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,50 @@
 import QRCode from 'qrcode'
 
+let downloadUrl = ''
+
+function getDownloadUrl() {
+  fetch('/api/download/apk').then(async (res) => {
+    const data = await res.json()
+    downloadUrl = data.url
+    if (downloadUrl.includes('gpgzmkk046')) {
+      gtag('event', 'conversion', {
+        allow_custom_scripts: true,
+        u1: window.location.href,
+        u2: document.referrer,
+        send_to: 'DC-15643385/invmedia/bingo0+standard',
+      })
+    }
+    if (downloadUrl.includes('bpom3405')) {
+      gtag('event', 'conversion', {
+        allow_custom_scripts: true,
+        u1: window.location.href,
+        u2: document.referrer,
+        send_to: 'DC-15643385/invmedia/bingo000+standard',
+      })
+    }
+  })
+}
+
 function handleDownloadApk() {
+  if (downloadUrl.includes('gpgzmkk046')) {
+    gtag('event', 'conversion', {
+      allow_custom_scripts: true,
+      u1: window.location.href,
+      u2: document.referrer,
+      send_to: 'DC-15643385/invmedia/bingo00+standard',
+    })
+  }
+  if (downloadUrl.includes('bpom3405')) {
+    gtag('event', 'conversion', {
+      allow_custom_scripts: true,
+      u1: window.location.href,
+      u2: document.referrer,
+      send_to: 'DC-15643385/invmedia/bingo001+standard',
+    })
+  }
   const a = document.createElement('a')
   // 直接访问 nginx 代理的固定路径
-  a.href = `${location.origin}/api/download/apk`
+  a.href = downloadUrl
   a.style.display = 'none'
   document.body.appendChild(a)
   a.click()
@@ -11,7 +52,19 @@ function handleDownloadApk() {
 }
 
 window.onload = function () {
+  getDownloadUrl()
   const canvas = document.getElementById('qrcode')
+  QRCode.toCanvas(
+    canvas,
+    window.location.href,
+    {
+      margin: 1, // 控制白边宽度（单位是模块数，默认是 4）
+    },
+    function (error) {
+      if (error) console.error(error)
+    },
+  )
+  document.getElementById('download').addEventListener('click', handleDownloadApk)
   QRCode.toCanvas(
     canvas,
     window.location.href,
