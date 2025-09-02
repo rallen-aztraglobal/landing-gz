24629553cb21942dde10884124bbe8c641f9210d
allen <allen.yho@yahoo.com>
2025-09-02 20:53:24 +0800

    coolgame2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,6 @@
 import QRCode from 'qrcode'
-import { initGtag } from './utils.js'
 
 function handleDownloadApk() {
-  gtag('event', 'conversion', {
-    allow_custom_scripts: true,
-    u1: window.location.href,
-    u2: document.referrer,
-    send_to: 'DC-15643385/invmedia/bingo00+standard',
-  })
-  console.log("✅ download  bingo00+standard")
   fetch('/api/download/apk').then(async (res) => {
     const data = await res.json()
     const a = document.createElement('a')
@@ -22,13 +14,6 @@ function handleDownloadApk() {
 }
 
 window.onload = function () {
-  gtag('event', 'conversion', {
-    allow_custom_scripts: true,
-    u1: window.location.href,
-    u2: document.referrer,
-    send_to: 'DC-15643385/invmedia/bingo0+standard',
-  })
-  console.log("✅ PV  bingo0+standard")
   const canvas = document.getElementById('qrcode')
   QRCode.toCanvas(
     canvas,
